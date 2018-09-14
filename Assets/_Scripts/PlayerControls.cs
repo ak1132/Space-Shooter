830c6147827a94e03653ba4049e0811b0c7ee061
DESKTOP-FA6IQA1\Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2018-09-14 02:09:32 -0400

    commit done

---
+++ Assets/_Scripts/PlayerControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate;
+    private float nextFire = 0.0f;
+
+    private void Update()
+    {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation) ;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = movement * speed;
+        rigidBody.position = new Vector3(
+            Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax), 
+            0.0f,
+            Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+        );
+        rigidBody.rotation = Quaternion.Euler(new Vector3(0.0f,0.0f, rigidBody.position.x * -tilt));
+	}
+
+
+}
